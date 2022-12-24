773fcc4c1f6dd1f4b9c95a1d463a7e7e451797f9
Mohsen3451 <m.hajian3451@gmail.com>
2022-12-24 19:56:36 +0330

    init project

--- App.js
+++ App.js
@@ -1,40 +1,14 @@
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-} from 'react-native';
-
-
-
-
+import {StyleSheet, View} from 'react-native';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <Text>hiiiiiiiiffffffffffffffiiiiiii</Text>
-    </SafeAreaView>
-  );
+  const onChangeSearch = () => {
+    console.log('ok');
+  };
+
+  return <View />;
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
